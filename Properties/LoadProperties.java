cefe73988003336da31efcacf8757d8147e51643
developthinking <wutihome@outlook.com>
2017-10-24 16:55:33 +0800

    Properties对象工具类

---
+++ Properties/LoadProperties.java
@@ -0,0 +1,48 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+public class LoadProperties {
+	/**
+	 * 指定配置文件信息读取
+	 * @return
+	 */
+    public static Properties getProperties(String fileNameIn) {
+    	//默认文件名称
+    	String fileName = "property.properties";
+    	if (StringUtils.isNotBlank(fileNameIn)) {
+    		fileName = fileNameIn;
+    	}
+
+        InputStream inputStream = null;
+
+        try {
+        	inputStream = ClassLoader.getSystemResourceAsStream(fileName);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Properties properties = new Properties();
+        try {
+            properties.load(new InputStreamReader(inputStream, "UTF-8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return properties;
+    }
+    /**
+     * 配置信息读取
+     * @param fileNameIn
+     * @return
+     */
+    public static String getInfoByKey(String fileNameIn, String propertyKey) {
+    	//配置信息读取
+    	Properties properties = getProperties(fileNameIn);
+    	//配置关键字读取
+        return properties.getProperty(propertyKey);
+    }
+}
