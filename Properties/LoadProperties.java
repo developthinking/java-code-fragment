59342828ce02dc962f8bb90a28ef7a1ff947d838
developthinking <wutihome@outlook.com>
2018-01-18 18:07:56 +0800

    properties工具类

--- Properties/LoadProperties.java
+++ Properties/LoadProperties.java
@@ -1,48 +1,88 @@
+package test.propertiesTest;
+
 import org.apache.commons.lang3.StringUtils;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Properties;
+import java.util.Set;
+
 
 public class LoadProperties {
 	/**
-	 * 指定配置文件信息读取
-	 * @return
+	 * 读Properties文件
+     * @param filePath 文件路径
+     * @param encoding 文件编码
 	 */
-    public static Properties getProperties(String fileNameIn) {
-    	//默认文件名称
-    	String fileName = "property.properties";
-    	if (StringUtils.isNotBlank(fileNameIn)) {
-    		fileName = fileNameIn;
+    public static Properties getProperties(String filePath, String encoding) {
+    	if (StringUtils.isNotBlank(filePath)) {
+    		return null;
     	}
-
         InputStream inputStream = null;
-
         try {
-        	inputStream = ClassLoader.getSystemResourceAsStream(fileName);
+            //inputStream = new FileInputStream(fileName);
+            inputStream = LoadProperties.class.getClassLoader().getResourceAsStream(filePath);
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         Properties properties = new Properties();
         try {
-            properties.load(new InputStreamReader(inputStream, "UTF-8"));
+            properties.load(new InputStreamReader(inputStream, encoding));
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return properties;
     }
+
     /**
-     * 配置信息读取
-     * @param fileNameIn
-     * @return
+     * 读Properties文件（有序）
+     * @param filePath 文件路径
+     * @param encoding 文件编码
      */
-    public static String getInfoByKey(String fileNameIn, String propertyKey) {
+    public static Properties getOrderedProperties(String filePath, String encoding) {
+        Properties properties = new OrderedProperties();
+        InputStreamReader inputStreamReader = null;
+        try {
+            InputStream inputStream = new BufferedInputStream(new FileInputStream(filePath));
+            //prop.load(in);//直接这么写，如果properties文件中有汉子，则汉字会乱码。因为未设置编码格式。
+            inputStreamReader = new InputStreamReader(inputStream, encoding);
+            properties.load(inputStreamReader);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStreamReader != null) {
+                try {
+                    inputStreamReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return properties;
+    }
+
+    /**
+     * 读取配置文件key对应的value
+     * @param filePath 文件路径
+     * @param encoding 文件编码
+     * @param propertyKey 配置文件key
+     */
+    public static String getInfoByKey(String filePath, String encoding, String propertyKey) {
     	//配置信息读取
-    	Properties properties = getProperties(fileNameIn);
+    	Properties properties = getProperties(filePath, encoding);
     	//配置关键字读取
         return properties.getProperty(propertyKey);
     }
+
+    /**
+     * 读取配置文件的key集合
+     * @param filePath 文件路径
+     * @param encoding 文件编码
+     */
+    public static Set<Object> getKeys(String filePath, String encoding) {
+        //配置信息读取
+        Properties properties = getProperties(filePath, encoding);
+        //关键字列表
+        return properties.keySet();
+    }
+
 }
